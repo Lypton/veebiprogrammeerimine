1ce8f24853e8515d193f83fbcba7b7c2e3075e12
Kenet Paurmann <lypton@users.noreply.github.com>
2019-09-10 11:09:16 +0300

    Esimene üleslaadimine

---
+++ 02 Unit/testpage.php
@@ -0,0 +1,47 @@
+<?php
+	$userName = "Lypton";
+
+?>
+
+<!doctype html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>
+<?php
+	echo $userName;
+?>
+
+	Veebiprogrammeerimine</title>
+
+</head>
+<body>
+	<div id="kast">
+		<style>
+		.kast {
+		background-color: black;
+		color: white;
+		padding: 5px;
+		height: 20px;
+		width:280px;
+
+	}
+</style>
+
+	<?php
+		echo "<h1>" .$userName .", veebiprogrammeerimine</h1>"
+	?>
+
+	<div>
+</div>
+	<p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tösiselt võetavat sisu</p>
+	<br><br>
+		<h3> Sotsiaalmeedia kontode loend:</h3>
+	<ul>
+		<li> <a href="https://www.instagram.com/kenetpaurmann"> Instagram - Kenet Paurmann</li>
+		<li> <a href="https://www.instagram.com/martentreimn"> Instagram - Märten Treimann</li>
+	</ul>
+
+
+</body>
+</html>
