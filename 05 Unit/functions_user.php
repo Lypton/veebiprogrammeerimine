12550039f99bcf1286424b83e480e5bf30a8f3fa
Kenet Paurmann <lypton@users.noreply.github.com>
2019-10-01 11:52:41 +0300

    kasutaja loomine, newuser.php line 87 veaga

---
+++ 05 Unit/functions_user.php
@@ -0,0 +1,22 @@
+<?php
+	function signUp($name, $surname, $email, $gender, $birthDate, $password){
+		$notice = null;
+		$conn = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $conn -> prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES(?,?,?,?.?,?)");
+		echo $conn->error;
+		//parooli krüpteering
+		$options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
+		$pwdhash = password_hash($password, PASSWORD_BRCYPT, $options);
+		$stmt->bind_param("sssiss", $name, $surname, $birthDate, $gender, $email, $pwdhash);
+		if($stmt->execute()){
+			$notice = "Kasutaja loomine õnnestus!";
+		} else {
+			$notice = "Kasutaja loomisel tekkis tehniline viga: " .$stmt->error;
+		}
+
+
+
+		$stmt -> close();
+		$conn -> close();
+		return $notice;
+	}
